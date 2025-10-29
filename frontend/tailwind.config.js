f9d786c8940003fdf8d94fc12844cdf8c0ce65ca
tautvis25 <tautvydastiskus@gmail.com>
2025-10-30 01:27:00 +0200

    updated visuals

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Scans all JS, TS, JSX, TSX files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
